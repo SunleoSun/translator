bf35ae4d0273a8599925429c4e5c2fb37dc04d1f
SuleoSun <SunleoSun123@gmail.com>
2014-07-02 13:41:33 +0300

    init

---
+++ Hooks/Class1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Volumer
+{
+    delegate int HookProc(int nCode, int wParam, IntPtr lParam);
+    public delegate void KeyHooksEventHandler(object sender, KeyHooksEventArgs e);
+    public class KeyHooksEventArgs
+    {
+        public bool control;
+    }
+
+    public class HookKeys
+    {
+
+        #region DllImports
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        static extern int SetWindowsHookEx(
+            int idHook,
+            HookProc lpfn,
+            IntPtr hMod,
+            int dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        static extern int UnhookWindowsHookEx(int idHook);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+             CallingConvention = CallingConvention.StdCall)]
+        static extern int CallNextHookEx(
+           int idHook,
+           int nCode,
+           int wParam,
+           IntPtr lParam);
+
+        [DllImport("user32")]
+        static extern int ToAscii(
+            int uVirtKey,
+            int uScanCode,
+            byte[] lpbKeyState,
+            byte[] lpwTransKey,
+            int fuState);
+
+        [DllImport("user32")]
+        static extern int GetKeyboardState(byte[] pbKeyState);
+        [DllImport("user32")]
+        static extern int GetAsyncKeyState(int pbKeyState);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        static extern short GetKeyState(int vKey);
+
+        #endregion
+
+        const int WH_KEYBOARD_LL = 13;
+        const int VK_LCONTROL = 0xA2;
+        const int VK_RCONTROL = 0xA3;
+        const int VK_LMENU = 0xA4;
+        const int VK_LEFT = 0x25;
+        const int VK_RIGHT = 0x27;
+        const int VK_LALT = 0xA4;
+        const int VK_RALT = 0xA5;
+        const int VK_T = 0x54;
+
+        HookProc _hookCallback;
+        private bool _isStarted;
+        int _handleToHook;
+
+        public void Start()
+        {
+            _hookCallback = new HookProc(HookCallbackProcedure);
+            _handleToHook = SetWindowsHookEx(
+                13,
+                _hookCallback,
+               Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]),
+                0);
+            int i = Marshal.GetLastWin32Error();
+            if (_handleToHook != 0)
+            {
+                _isStarted = true;
+            }
+        }
+        public void Stop()
+        {
+            if (_isStarted)
+            {
+                UnhookWindowsHookEx(_handleToHook);
+                _isStarted = false;
+            }
+        }
+        public event KeyHooksEventHandler KeyHooked;
+
+        void OnKeyHooks(KeyHooksEventArgs e)
+        {
+            KeyHooksEventHandler del = KeyHooked;
+            if (del != null)
+                del(this, e);
+        }
+        public int HookCallbackProcedure(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            bool control = ((GetKeyState(VK_LCONTROL) & 0x80) != 0) &&
+                       ((GetKeyState(VK_RCONTROL) & 0x80) != 0);
+            KeyHooksEventArgs arg = new KeyHooksEventArgs();
+            arg.control = control;
+            OnKeyHooks(arg);
+            return CallNextHookEx(_handleToHook, nCode, wParam, lParam);
+
+        }
+
+    }
+}
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Volumer;
+using System.Net;
+using System.IO;
+namespace TranslatorMainApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HookKeys hk = new HookKeys();
+        NotifyIcon nf = new NotifyIcon();
+        private void MenuClickExit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void hk_KeyHooked(object sender, KeyHooksEventArgs e)
+        {
+            if (e.control)
+            {
+                nf.BalloonTipText = Translate(Clipboard.GetText(TextDataFormat.Text), "en", "ru");
+                nf.ShowBalloonTip(1000);
+            }
+        }
+
+        private static string Translate(string InputText, string FromLang, string ToLang)
+        {
+            string OutputText = "";
+            WebRequest req = WebRequest.Create(@"http://api.microsofttranslator.com/V2/Http.svc/Translate?appId=FD9BE24FE82B92F679959684D22AEB0B14752498&text=" + InputText + "&from=" + FromLang + "&to=" + ToLang);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            OutputText = sr.ReadToEnd();
+            OutputText = OutputText.Remove(OutputText.Length - 9);
+            OutputText = OutputText.Remove(0, 68);
+            return OutputText;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Bitmap bitmap = TranslatorMainApplication.Properties.Resources._1;
+            nf.Icon = new System.Drawing.Icon(Icon.FromHandle(bitmap.GetHicon()), new Size(32, 31));
+            nf.BalloonTipIcon = ToolTipIcon.Info;
+            nf.Visible = true;
+            ContextMenu.MenuItemCollection menu = new ContextMenu.MenuItemCollection(nf.ContextMenu = new ContextMenu());
+            menu.Add("Exit");
+            menu[0].Click += new EventHandler(MenuClickExit);
+            hk.KeyHooked += new KeyHooksEventHandler(hk_KeyHooked);
+            hk.Start();
+
+        }
+
+        private void Closing(object sender, FormClosingEventArgs e)
+        {
+            hk.Stop();
+        }
+
+        private void Showned(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+
+    }
+}
